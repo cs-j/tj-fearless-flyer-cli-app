9057ea62b663210058fb4478730979ed614bd487
Camille <cj@mail.co.de>
2017-05-15 13:29:08 -0400

    Delete best_sellers.rb

--- lib/best_sellers.rb
+++
@@ -1,8 +0,0 @@
-require 'nokogiri'
-require 'open-uri'
-# Remove this pry requirement once you're ready to publish gem
-require 'pry'
-
-require_relative "./best_sellers/version"
-require_relative './best_sellers/cli'
-require_relative './best_sellers/book'
